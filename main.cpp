5387005b518c55e2fc08e5ed5f6f8a5734b6425e
Zedmous <ejnsilva29@gmail.com>
2024-06-17 20:29:36 -0400

    preparacion del enunciado

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 /* eduardo nieves */
 
+
+/*
+Hacer un programa que lea los datos de una persona, los cuales son  nombre, apellido, fecha de nacimiento, sexo, peso, altura.
+El programa debe lo siguiente:
+ calcular la edad
+ verificar si esa persona es mayor de edad
+ cuantas personas son de sexo masculino y femenino	
+*/
+
+
+
 int main(int argc, char** argv) {
 	return 0;
 }
